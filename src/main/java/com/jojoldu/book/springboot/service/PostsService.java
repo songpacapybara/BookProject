46f9c1af15c859ac7b2fb90c6f19f09599cbe8bd
songpacapybara <klose1234@naver.com>
2024-07-26 13:19:43 +0900

    7/26 commit

--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -2,12 +2,17 @@ package com.jojoldu.book.springboot.service;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,4 +40,15 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // -> 람다식/ 실제 코드는 map.(posts -> new PostsListResponseDto(posts))
+                                                // postsRepository 결과로 넘어온 posts의 stream을 map을 통해 List로 반환하는 메소드
+                .collect(Collectors.toList());
+
+    }
 }
+// -> 람다식/ 실제 코드는 map.(posts -> new PostsListResponseDto(posts))
+// postsRepository 결과로 넘어온 posts의 stream을 map을 통해 List로 반환하는 메소드
---
+++ src/main/resources/templates/static/js/app/index.js
@@ -0,0 +1,31 @@
+var main = {
+    //브라우저의 스코프는 공용공간으로 쓰이기 때문에 나중에 로딩된 js의 init,save가 먼저 로딩된 js의 function을 덮어 쓰게 된다.
+    //var index란 객체를 생성해 해당 객체에서 필요한 모든 function을 선언하여 다른 js와 겹칠 위험이 사라진다.
+    init: function () {
+        var _this = this;
+        $('#btn-save').on('click', function() {
+            _this.save();
+        });
+    },
+    save: function() {
+        var data = {
+            title: $('#title').val(),
+            author: $('#author').val(),
+            content: $('#content').val()
+        };
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+main.init();
